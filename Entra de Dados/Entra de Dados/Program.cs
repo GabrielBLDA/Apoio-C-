88463679edff30dadeec753acc0da5e8b8521d09
Gabriel Abreu <GabrielAbreudeLima1@gmail.com>
2023-01-27 21:53:26 -0300

    add 27/11/23

---
+++ Entra de Dados/Entra de Dados/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra_de_Dados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o seu nome completo:");       
+            string Nome = Console.ReadLine();
+            Console.WriteLine("Seja Bem Vindo " + Nome);
+            Console.WriteLine("Insira a Sua Idade");
+            string Idade = Console.ReadLine();
+            var IdadeInt = Convert.ToInt32(Idade);
+
+            if (IdadeInt < 18)
+            {
+                Console.WriteLine("Você é menor de idade");
+            } else
+            {
+                Console.WriteLine("Você é maior de idade");
+            }
+
+            Console.WriteLine("Pressione qualquer Tecla:");
+            char teclaPressionada = Console.ReadKey(true).KeyChar;
+
+            Console.WriteLine("A tecla pressionado foi: " + teclaPressionada);
+
+            Console.WriteLine("Pressione Qualquer tecla para sair");
+            Console.ReadKey();
+        } 
+    }
+}
