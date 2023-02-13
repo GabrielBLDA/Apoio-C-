02f512de4ec4685f0138e23e9b5219f702958e06
Gabriel Abreu <GabrielAbreudeLima1@gmail.com>
2023-02-12 21:22:30 -0300

    add in 12/02

---
+++ laço de repetição - break e continue/laço de repetição - break e continue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laço_de_repetição___break_e_continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Pressione C para Continue");
+                Console.WriteLine("Pressione E para Break");
+                string tecla = Console.ReadKey(true).KeyChar.ToString();
+                if(tecla == "c")
+                {
+                    continue;
+                }
+                else if (tecla == "e")  
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Tecla desconhecida");
+                }
+                Console.WriteLine("Pressione qualquer tecla para continuar");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            Console.WriteLine("Pressione qualquer tecla para Sair");
+            Console.ReadKey();
+        }
+    }
+}
