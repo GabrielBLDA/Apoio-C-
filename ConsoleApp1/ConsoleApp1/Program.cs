c4b5918a7cbb6bfb404034706a5e0901f67df2bb
Gabriel Abreu <GabrielAbreudeLima1@gmail.com>
2023-02-15 18:58:32 -0300

    add in 15/02

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Digite S para sair ou um numero para calcular o fatorial");
+                string opcao = Console.ReadLine();
+                if (opcao == "s" || opcao == "S")
+                break;
+
+                else
+                {
+                    int valor = Convert.ToInt32(opcao);
+                    if (valor == 0)
+                    {
+                        Console.WriteLine("0! = 1");
+                    }else if (valor<0)
+                    {
+                        Console.WriteLine("impossivel calcular o fatorial de um numero negativo");
+                    }else
+                    {
+                        int resultado = 1;
+                        for (int i = valor; i > 0; i--)
+                        {
+                            resultado *= i;
+                        }
+                        Console.WriteLine($"O resultado de {valor} fatorial é: {resultado}");
+                    }
+                    Console.WriteLine("Pressione qualquer tecla para continuar");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            Console.WriteLine("Pressione qualquer tecla para finalizar");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProjetoLoginConsole/ProjetoLoginConsole/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoLoginConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+                Console.Write("Digite o nome do seu usuario: ");
+                string usuario = Console.ReadLine();
+                Console.Write("Digite a sua senha: ");
+                string senha = "";
+                while (true)
+                {
+                    ConsoleKeyInfo tecla = Console.ReadKey(true);
+                    if (tecla.Key == ConsoleKey.Enter)
+                        break;
+
+                    else
+                    {
+                        senha += tecla.KeyChar;
+                    }
+                }
+                if (usuario == "Valeria" && senha == "1234")
+                {
+                    Console.WriteLine("usuario logado com sucesso");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Usario ou senha incorreto");
+                    Console.WriteLine("Pressione qualquer tecla para continuar");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                Console.WriteLine("Pressione Qualquer tecla para sair");
+                Console.ReadKey();
+            }
+          
+
+        }
+    }
+}
