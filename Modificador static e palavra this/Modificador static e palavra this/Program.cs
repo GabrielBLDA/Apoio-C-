9d3020014692aa0a37b488a598bc74f90482144e
Gabriel Abreu <GabrielAbreudeLima1@gmail.com>
2023-02-17 19:22:52 -0300

    POO ATTS

---
+++ Modificador static e palavra this/Modificador static e palavra this/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modificador_static_e_palavra_this
+{
+    //Modificador static (estatico ou fixo) - Ele é sempre usado apos o modificador de acesso
+    //O static transforma o que esta sendo modificado por ele em pertencente á classe e não mais ao objeto.
+    //Pode ser usado em:
+    //atributo
+    //propriedade
+    //metodo
+    //struct
+    //classe
+    //...
+
+    //Não tem nada estatico na classe pessoa
+    public class Pessoa
+    {
+        //atributo
+        private string nome;
+
+        //propriedade de classe
+        public string Nome { get; set; }
+
+
+        //metodo
+        public void ImprimeNome()
+        {
+            Console.WriteLine($"O nome é {nome}");
+        }
+        //metodo construtor
+
+        public Pessoa(string pNome)
+        {
+            nome = pNome;
+        }
+    }
+
+    //Palavra this (este) - é utilizado para se referir aquilo que pertence a nossa classe
+    public class MinhaPessoa
+    {
+        //atributo
+        private string nome;
+
+        //propriedade de classe
+        public string Nome { get; set; }
+
+
+        //metodo
+        public void ImprimeNome()
+        {
+            Console.WriteLine($"O nome é {nome}");
+        }
+
+        //metodo construtor
+        public MinhaPessoa(string nome)
+        {
+            this.nome = nome;
+        }
+    }
+
+    //A classe calculadora possue o modificador static
+    public class Calculadora
+    {
+            public static double PI = 3.14;
+
+            public static double CalculaAreaCircunferencia(double pRaio)
+            {
+                return PI * Math.Pow(pRaio, 2);
+            }     
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa("João");
+            Pessoa pessoa2 = new Pessoa("Pedro");
+
+            pessoa1.ImprimeNome();
+            pessoa2.ImprimeNome();
+
+            //chamar static só pode ser acessaado apartir do nome da classe, pois ele pertence a classe
+            Console.WriteLine($"O valor de PI é: {Calculadora.PI}");
+            Console.WriteLine($"A area é {Calculadora.CalculaAreaCircunferencia(5)}");
+            Calculadora calc = new Calculadora();
+           
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Testando Conhecimento (POO)/Testando Conhecimento/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testando_Conhecimento
+{
+    internal class Program
+    {
+        public class Locadora
+        {
+            //Atributos
+            public string nomeCliente;
+            public string senha;
+            public bool situacaoAluguel;
+
+
+            //metodos
+            public void AnalisarSituação(string pSenha)
+            {
+                if(pSenha == senha && situacaoAluguel == true)
+                {
+                    Console.WriteLine($"{nomeCliente} está apto a alugar um filme em nossa locadora");
+                   
+                }
+                else if (pSenha != senha)
+                {
+                     Console.WriteLine("A senha inserida esta incorreta"); 
+                }
+                else
+                {
+                    Console.WriteLine("Você ainda não pode alugar nenhum filme em nossa locadora, pois ainda não devolveu o ultimo");
+                }
+
+            }  
+
+            public void DevolverFilme(string pSenha)
+            {
+                if(pSenha == senha)
+                {
+                     
+                    Console.WriteLine($"Acesso autorizado! seja bem vindo {nomeCliente}");
+                    if(situacaoAluguel != true)
+                    {
+                        Console.WriteLine($"Você devolveu seu filme! muito obrigado!");
+                        situacaoAluguel = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Você Não tem Filmes para devolver no momento!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Senha incorreta!");
+                }
+            }
+
+            public void AlugarFilme(string psenha)
+            {
+                if (psenha == senha)
+                {
+                    Console.WriteLine($"Acesso autorizado! seja bem vindo {nomeCliente}");
+                    if (situacaoAluguel != true)
+                    {
+                        Console.WriteLine($"Você Não pode alugar um filme");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Você Alugou um filme!");
+                        situacaoAluguel = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Senha incorreta!");
+                }
+            }
+
+            public Locadora (string pSenha, string pNomeCliente) //METODO CONSTRUTOR
+            {
+                senha = pSenha;
+                nomeCliente = pNomeCliente;
+                situacaoAluguel = true;
+            }
+     
+        }
+        static void Main(string[] args)
+        {
+            Locadora ContaBilada = new Locadora("12345", "Gabriel Abreu de Lima"); //Criando obj
+
+            ContaBilada.AnalisarSituação("12345");
+            Console.WriteLine(); 
+
+            ContaBilada.DevolverFilme("12345");
+            Console.WriteLine();
+
+            ContaBilada.AlugarFilme("12345");
+            Console.WriteLine();
+
+            ContaBilada.DevolverFilme("12345");
+            Console.WriteLine();
+
+            ContaBilada.DevolverFilme("12345");
+            Console.WriteLine();
+
+            ContaBilada.AlugarFilme("12345");
+            Console.WriteLine();
+
+            ContaBilada.AlugarFilme("12345");
+            Console.WriteLine();
+
+            ContaBilada.AnalisarSituação("12345");
+            Console.WriteLine();
+
+            ContaBilada.DevolverFilme("12345");
+            Console.WriteLine();
+
+            ContaBilada.AnalisarSituação("12345");
+            Console.WriteLine();
+
+            ContaBilada.DevolverFilme("12345");
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+        }
+    }
+}
